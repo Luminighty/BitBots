31b11871f9044c3d2bd27679cb474f4d8ba97ff1
Tsohg <asaneoil@gmail.com>
2021-03-28 09:26:28 -0400

    Unit Movement

---
+++ src/game/settings.ts
@@ -0,0 +1,5 @@
+export const Settings = {
+  //Map Boundaries.
+  BOUNDS_X: 100,
+  BOUNDS_Y: 100
+}
--- test/unit_tests.ts
+++ test/unit_tests.ts
@@ -1,10 +1,73 @@
-import { Unit } from "../src/game/unit";
-var assert = require('assert');
+import {Unit} from "../src/game/unit";
+import {Vector2} from "../src/game/data_structures/vector2";
+import {Settings} from "../src/game/settings";
 
-describe('bot_tests', function() {
-  describe('moveRight()', function() {
-    it('should move the bot right 1 space.', function() {
+var assert = require("assert");
+
+describe("bot_tests", function() {
+  describe("moving", function() {
+    it("should move the bot on the map within its bounds if it is able to move.", function() {
         var u = new Unit();
+
+        //Test chunk 1
+        u.setPosition(0, 1);
+        u.setCanMove(true);
+        u.moveUp();
+        assert.equal(u.getPosition().y, 2, "chunk 1: up");
+        u.moveRight();
+        assert.equal(u.getPosition().x, 1, "chunk 1: right");
+        u.moveLeft();
+        assert.equal(u.getPosition().x, 0, "chunk 1: left");
+        u.moveDown();
+        assert.equal(u.getPosition().y, 1, "chunk 1: down");
+
+        //Test chunk 2
+        u.setPosition(0, 0);
+        u.moveUp();
+        u.moveUp();
+        u.moveUp();
+        u.moveRight();
+        u.moveRight();
+        assert.equal(u.getPosition().x, 2, "chunk 2: right");
+        assert.equal(u.getPosition().y, 3, "chunk 2: up");
+
+        //Test chunk 3
+        for(var i = 0; i < Settings.BOUNDS_X + 10; i++)
+          u.moveLeft();
+        assert.equal(u.getPosition().x, 0, "chunk 3: left");
+        for(var i = 0; i < Settings.BOUNDS_X + 10; i++)
+          u.moveRight();
+        assert.equal(u.getPosition().x, Settings.BOUNDS_X, "chunk 3: right");
+        for(var i = 0; i < Settings.BOUNDS_Y + 10; i++)
+          u.moveDown();
+        assert.equal(u.getPosition().y, 0, "chunk 3: down");
+        for(var i = 0; i < Settings.BOUNDS_Y + 10; i++)
+          u.moveUp();
+        assert.equal(u.getPosition().x, Settings.BOUNDS_Y, "chunk 3: up");
+
+        //Test chunk 4
+        u.setPosition(1, 1);
+        u.setCanMove(false);
+        u.moveUp();
+        assert.equal(u.getPosition().y, 1, "chunk 4: up");
+        u.moveRight();
+        assert.equal(u.getPosition().x, 1, "chunk 4: right");
+        u.moveLeft();
+        assert.equal(u.getPosition().x, 1, "chunk 4: left");
+        u.moveDown();
+        assert.equal(u.getPosition().y, 1, "chunk 4: down");
+
+        //Test chunk 5
+        u.setPosition(-1, -1);
+        u.setCanMove(true);
+        u.moveUp();
+        u.moveRight();
+        assert.equal(u.getPosition().x, -1, "chunk 5: right");
+        assert.equal(u.getPosition().y, -1, "chunk 5: up");
+        u.moveDown();
+        u.moveLeft();
+        assert.equal(u.getPosition().x, -1, "chunk 5: left");
+        assert.equal(u.getPosition().y, -1, "chunk 5: down");
     })
-  })
-})
+  }) //end moving tests
+}) //end unit_tests
