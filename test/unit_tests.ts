20596842eafaf0264f92ff61871ffec62adcf833
Tsohg <asaneoil@gmail.com>
2021-03-28 07:05:08 -0400

    Typescript-Mocha Unit testing.

---
+++ test/unit_tests.ts
@@ -0,0 +1,10 @@
+import { Unit } from "../src/game/unit";
+var assert = require('assert');
+
+describe('bot_tests', function() {
+  describe('moveRight()', function() {
+    it('should move the bot right 1 space.', function() {
+        var u = new Unit();
+    })
+  })
+})
