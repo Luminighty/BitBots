2b79d85c373764655219724c45a9922322bec8af
Luminight <balazscsabai98@gmail.com>
2021-03-29 21:41:53 +0200

    rocket init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::response::content;
+
+#[get("/")]
+fn index() -> content::Json<&'static str> {
+    content::Json("{ \"hi\": \"world\" }")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
